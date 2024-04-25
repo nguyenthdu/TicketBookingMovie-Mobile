7b4674a0aa5d625b116a25df0e7bb7bd6ce3a037
Nguyễn Xuân An <117873016+NguyenXuanAn5161@users.noreply.github.com>
2024-04-25 19:03:33 +0700

    thêm tính năng thông báo nhận được khuyến mãi vé (ghế), đồ ăn

--- src/components/Booking/BookingSummary.js
+++ src/components/Booking/BookingSummary.js
@@ -1,11 +1,17 @@
 import React, { useEffect, useState } from "react";
-import { Text, View } from "react-native";
+import { Alert, Text, View } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
 import {
   doSetSelectedPromotionBill,
+  doSetSelectedPromotionFood,
+  doSetSelectedPromotionSeat,
   doSetSelectedRoom,
 } from "../../redux/booking/bookingSlice";
-import { fetchPromotionByBill } from "../../services/PromotionAPI";
+import {
+  fetchPromotionByBill,
+  fetchPromotionByFood,
+  fetchPromotionByTicket,
+} from "../../services/PromotionAPI";
 import { callFetchRoomById } from "../../services/RoomAPI";
 import { calculateTotalPrice } from "../../utils/bookingUtils";
 import { formatCurrency } from "../../utils/formatData";
@@ -18,9 +24,16 @@ const BookingSummary = () => {
   const selectedPromotionBill = useSelector(
     (state) => state.booking.selectedPromotionBill
   );
+  const selectedPromotionSeat = useSelector(
+    (state) => state.booking.selectedPromotionSeat
+  );
+  const selectedPromotionFood = useSelector(
+    (state) => state.booking.selectedPromotionFood
+  );
   const selectedShowTime = useSelector(
     (state) => state.booking.selectedShowTime
   );
+  const selectedCinema = useSelector((state) => state.booking.selectedCinema);
 
   const [roomPrice, setRoomPrice] = useState(0);
   const [totalPrice, setTotalPrice] = useState(0);
@@ -34,7 +47,6 @@ const BookingSummary = () => {
 
   const fetchRoomById = async (id) => {
     const res = await callFetchRoomById(id);
-    console.log("res price: ", res);
     if (res) {
       setRoomPrice(res.price);
       dispatch(doSetSelectedRoom(res));
@@ -67,8 +79,65 @@ const BookingSummary = () => {
     const resPromotion = await fetchPromotionByBill(price);
     if (resPromotion) {
       if (resPromotion.id !== selectedPromotionBill?.id) {
+        Alert.alert(
+          "Thông báo",
+          `Bạn nhận được khuyến mãi ${resPromotion.name} với giá trị ${
+            resPromotion.promotionDiscountDetailDto.discountValue
+          }${
+            resPromotion.promotionDiscountDetailDto.typeDiscount === "PERCENT"
+              ? "%"
+              : "đ"
+          } giảm tối đa ${formatCurrency(
+            resPromotion.promotionDiscountDetailDto.maxValue
+          )}`
+        );
         console.log("so sanh: ", resPromotion.id, selectedPromotionBill.id);
         dispatch(doSetSelectedPromotionBill(resPromotion));
+      }
+    }
+  };
+
+  // fetch promotion by seat
+  useEffect(() => {
+    if (selectedSeats.length > 0 && selectedShowTime?.id) {
+      getPromotionByTicket(selectedSeats, selectedShowTime.id);
+    } else {
+      dispatch(doSetSelectedPromotionSeat({}));
+    }
+  }, [selectedSeats]);
+
+  const getPromotionByTicket = async (seats, showTimeId) => {
+    const resPromotion = await fetchPromotionByTicket(seats, showTimeId);
+    console.log("res promotion seat: ", resPromotion);
+    if (resPromotion) {
+      if (resPromotion.id !== selectedPromotionSeat?.id) {
+        Alert.alert(
+          "Thông báo",
+          `Bạn nhận được khuyến mãi ${resPromotion.name}`
+        );
+        dispatch(doSetSelectedPromotionSeat(resPromotion));
+      }
+    }
+  };
+
+  // fetch promotion by food
+  useEffect(() => {
+    if (selectedFoods.length > 0 && selectedCinema) {
+      getPromotionByFood(selectedFoods, selectedCinema);
+    } else {
+      dispatch(doSetSelectedPromotionFood({}));
+    }
+  }, [selectedFoods]);
+
+  const getPromotionByFood = async (foods, cinemaId) => {
+    const resPromotion = await fetchPromotionByFood(foods, cinemaId);
+    if (resPromotion) {
+      if (resPromotion.id !== selectedPromotionFood?.id) {
+        Alert.alert(
+          "Thông báo",
+          `Bạn nhận được khuyến mãi ${resPromotion.name}`
+        );
+        dispatch(doSetSelectedPromotionFood(resPromotion));
       }
     }
   };

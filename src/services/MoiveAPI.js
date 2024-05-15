bca8d5f23b3de33e64a9c309b7a82d9d836a168e
Nguyễn Xuân An <117873016+NguyenXuanAn5161@users.noreply.github.com>
2024-05-15 21:30:12 +0700

    fix typeShow cho api get movie

--- src/services/MoiveAPI.js
+++ src/services/MoiveAPI.js
@@ -2,7 +2,7 @@ import axios from "../utils/axios-custom";
 
 export const fetchMoviesTrending = async () => {
   try {
-    const response = await axios.get(`/api/movie?typeShow=Upcoming`);
+    const response = await axios.get(`/api/movie?typeShow=Showing`);
     return response.data.content;
   } catch (error) {
     console.error("Error fetching movies:", error);
